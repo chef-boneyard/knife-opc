c9e20778fb7f2d7e2727d105e196205368c8c45f
Shruthi <shruthiv@yahoo-inc.com>
2013-10-25 13:30:55 -0700

    test for org creation

--- spec/unit/opc_org_create_spec.rb
+++ spec/unit/opc_org_create_spec.rb
@@ -3,10 +3,23 @@ require File.expand_path('../../spec_helper', __FILE__)
 describe Opc::OpcOrgCreate do
   before :each do
     @knife = Chef::Knife::OpcOrgCreate.new
-    @stdout = StringIO.new
-    @stderr = StringIO.new
-    @knife.ui.stub(:stdout).and_return(@stdout)
-    @knife.ui.stub(:stderr).and_return(@stderr)
+    @rest = double('Chef::REST')
+    Chef::REST.stub(:new).and_return(@rest)
+    @org_name = 'ss'
+    @org_full_name = 'secretsauce'
+  end
+
+  let(:org_args) do
+    {
+      :name => @org_name, 
+      :full_name => @org_full_name
+    }
+  end
+
+  let(:result) do
+    {
+      :private_key => 'hello'
+    } 
   end
 
   describe 'with no org_name and org_fullname' do
@@ -14,6 +27,17 @@ describe Opc::OpcOrgCreate do
       @knife.ui.should_receive(:fatal).with("You must specify an ORG_NAME and an ORG_FULL_NAME")
       @knife.should_receive(:show_usage)
       lambda { @knife.run }.should raise_error( SystemExit )
+    end
+  end
+
+  describe '' do
+    before :each do
+      @knife.name_args << @org_name << @org_full_name
+    end
+    it 'should create an org' do 
+      @rest.should_receive(:post_rest).with('organizations/',org_args).and_return(result)
+      @knife.ui.should_receive(:msg).with(result['private_key'])
+      @knife.run
     end
   end
 end
