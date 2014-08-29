8007d7c5b2684f21b93b14ecefb96693670fd38d
Josh Glass <glass@getchef.com>
2014-08-29 13:09:18 -0500

    User Org association

---
+++ lib/chef/knife/opc_user_associate.rb
@@ -0,0 +1,41 @@
+#
+# Author:: Steven Danna (<steve@opscode.com>)
+# Copyright:: Copyright 2011 Opscode, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+module Opc
+  class OpcUserAssociate < Chef::Knife
+    category "OPSCODE PRIVATE CHEF ORGANIZATION MANAGEMENT"
+    banner "knife opc user associate USERNAME ORGNAME"
+
+    def run
+      case @name_args.count
+      when 2
+        username, orgname = @name_args
+      else
+        ui.fatal "Wrong number of arguments"
+        show_usage
+        exit 1
+      end
+
+      request_body = {:user => username}
+      response = @chef_rest.post_rest "organizations/#{config[:orgname]}/association_requests", request_body
+      association_id = response["uri"].split("/").last
+      @chef_rest.put_rest "users/#{username}/association_requests/#{association_id}", { :response => 'accept' }
+
+    end
+  end
+end
